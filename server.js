20ab82b373b9de94a33995fc680b9576bada0cfd
Armando Contreras <armandocontreras62596@gmail.com>
2021-01-02 19:55:47 -0800

    Added my listener to check that my app is working. I initally had a different syntax for my express.static method, but I changed it as I had an error when I tried to run my server

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
+
+app.listen(PORT, function () {
+    console.log("Server listening on PORT: " + PORT)
+})
