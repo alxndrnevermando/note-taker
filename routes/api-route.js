01bc55100348ad3b4f86c281ef28e14cb4b52b08
Armando Contreras <armandocontreras62596@gmail.com>
2021-01-02 21:17:49 -0800

    created variables that contain a require funciton for our database and to use the fs module

--- routes/api-route.js
+++ routes/api-route.js
@@ -4,8 +4,8 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+const database = require("../Develop/db/db.json");
+const fs = require("fs");
 
 
 // ===============================================================================
