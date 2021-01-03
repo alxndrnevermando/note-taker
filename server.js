449ad05ed5be535a9c20735836e24da075cff222
Armando Contreras <armandocontreras62596@gmail.com>
2021-01-02 23:46:04 -0800

    deleted the class readme file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 
 const PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '/public')));
 
 require("./routes/api-route")(app);
 require("./routes/html-route")(app);
