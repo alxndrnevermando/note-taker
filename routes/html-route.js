40a3811a75fc9d3d863c6c36f2bf84d58a22ae17
Armando Contreras <armandocontreras62596@gmail.com>
2021-01-02 20:40:09 -0800

    changed our routing in the thml route file so that it routes to my static pages, index.html and notes.html.

--- routes/html-route.js
+++ routes/html-route.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const path = require("path");
 
 
 // ===============================================================================
@@ -15,16 +15,12 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
-  });
-
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
   });
 };
