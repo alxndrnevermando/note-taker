f7aa6e0a900d471ce52d85cc55b0f38cc084ce73
Armando Contreras <armandocontreras62596@gmail.com>
2021-01-02 22:06:02 -0800

    added a variable called entry which is supposed to take our newly created notes in our post request.

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,7 +1,6 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// We are linking our routes to a series of "data"
 // ===============================================================================
 
 const database = require("../Develop/db/db.json");
@@ -24,24 +23,13 @@ module.exports = function(app) {
   });
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
+  app.post("/api/notes", function(req, res) {
+
+    let entry = {
+      title: req.body.title,
+      text: req.body.text
     }
   });
 
